6896ad78415794e7e7e20a0d8d82276db56704cc
Julian Garnica <jsgarnicacastillo@gmail.com>
2022-01-27 15:44:41 -0500

    arreglo posible bug

--- project/urls.py
+++ project/urls.py
@@ -10,6 +10,15 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index),
+    url(r'^health$', health),
     path('test/', views.testHolaMundo.as_view()),
+    
 ]
 
+
+if settings.DEBUG:
+  import debug_toolbar
+  urlpatterns = [
+      url(r'^__debug__/', include(debug_toolbar.urls)),
+  ] + urlpatterns
